5340738753ca310a90b7c8105630cee678fa6860
Rifqi Eka Hardianto <ekarifqi1@gmail.com>
2021-10-05 12:54:39 +0700

    add movie list item

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nonton/theme.dart';
 import 'package:nonton/widgets/movie_carousel_item.dart';
+import 'package:nonton/widgets/movie_list_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,80 +10,115 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 29,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 24,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Home',
-                      style: blackTextStyle.copyWith(
-                        fontSize: 28,
-                        fontWeight: black,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      'Watch much easier',
-                      style: greyTextStyle.copyWith(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Container(
-                  width: 55,
-                  height: 45,
-                  decoration: BoxDecoration(
-                    color: whiteColor,
-                    borderRadius: BorderRadius.horizontal(
-                      left: Radius.circular(259),
-                    ),
-                  ),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/icon_search.png',
-                      width: 22,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 29,
+              ),
+              Row(
                 children: [
-                  MovieCarouselItem(
-                    imageUrl: 'assets/image_movie1.png',
-                    title: 'John Wick 4',
-                    rating: 10,
-                    releaseDate: DateTime(2021, 6, 17),
+                  SizedBox(
+                    width: 24,
                   ),
-                  MovieCarouselItem(
-                    imageUrl: 'assets/image_movie2.png',
-                    title: 'Bohemian',
-                    rating: 8,
-                    releaseDate: DateTime(2020, 5, 11),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Home',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 28,
+                          fontWeight: black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        'Watch much easier',
+                        style: greyTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Container(
+                    width: 55,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: whiteColor,
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(259),
+                      ),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/icon_search.png',
+                        width: 22,
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    MovieCarouselItem(
+                      imageUrl: 'assets/image_movie1.png',
+                      title: 'John Wick 4',
+                      rating: 10,
+                      releaseDate: DateTime(2021, 6, 17),
+                    ),
+                    MovieCarouselItem(
+                      imageUrl: 'assets/image_movie2.png',
+                      title: 'Bohemian',
+                      rating: 8,
+                      releaseDate: DateTime(2020, 5, 11),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,
+                  left: 24,
+                  bottom: 20,
+                ),
+                child: Text(
+                  'From Disney',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: black,
+                  ),
+                ),
+              ),
+              MovieListItem(
+                imageUrl: 'assets/image_movie3.png',
+                title: 'Mulan Session',
+                rating: 6,
+                releaseDate: DateTime(2021, 6, 17),
+              ),
+              MovieListItem(
+                imageUrl: 'assets/image_movie4.png',
+                title: 'Beauty & Beast',
+                rating: 10,
+                releaseDate: DateTime(2021, 6, 17),
+              ),
+              MovieListItem(
+                imageUrl: 'assets/image_movie3.png',
+                title: 'Mulan Session',
+                rating: 6,
+                releaseDate: DateTime(2021, 6, 17),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/movie_list_item.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:nonton/theme.dart';
+
+class MovieListItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final DateTime releaseDate;
+  final double rating;
+
+  const MovieListItem({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    required this.releaseDate,
+    required this.rating,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: 24,
+        right: 24,
+        bottom: 30,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 100,
+            height: 127,
+            margin: EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  imageUrl,
+                ),
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: extraBold,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                DateFormat('MMM dd, yyyy').format(releaseDate),
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: rating >= 2 ? yellowColor : lightGreyColor,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 4 ? yellowColor : lightGreyColor,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 6 ? yellowColor : lightGreyColor,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 8 ? yellowColor : lightGreyColor,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: rating >= 10 ? yellowColor : lightGreyColor,
+                    size: 18,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
